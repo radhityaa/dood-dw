d3728cf295acb679d7e2701047202a25aee21f15
Rama Adhitya Setiadi <ramaadhityasetiadi002@gmail.com>
2025-03-09 14:14:26 +0700

    update-axios-create

--- pages/api/getDetail.ts
+++ pages/api/getDetail.ts
@@ -22,13 +22,13 @@ export default async function handler(
     const userAgent = req.headers['user-agent']
     const axiosClient = axios.create({
         headers: {
-            'User-Agent': userAgent,
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-            'Accept-Language': 'en-US,en;q=0.5',
+            'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36 Edg/134.0.0.0",
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'Accept-Language': 'en-US,en;q=0.9',
             'Referer': 'https://vidply.com',
-            'Sec-Fetch-Dest': 'empty',
-            'Sec-Fetch-Mode': 'cors',
-            'Sec-Fetch-Site': 'same-origin',
+            'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'none',
         }
     })
 
